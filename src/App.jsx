b9baa14fc34ef26419bd1c5da4b3a7cc8675043f
Steven <stevenaknobel@gmail.com>
2024-11-17 11:40:25 +0200

    created seperate componenets for the header and layout and linked into the app.jsx instead of having the header directly in the code

--- src/App.jsx
+++ src/App.jsx
@@ -18,28 +18,24 @@ import Home from "./pages/Home"
 import About from "./pages/About"
 import Vans from "./pages/Vans"
 import VanDetail from "./pages/VanDetail"
+import Layout from "./Components/Layout"
 
 import "./server"
 
 function App() {
 
   return (
-    <BrowserRouter>
-      <header>
-        <Link className="site-logo" to="/">#VanLife</Link>
-        <nav>
-          <Link to="/about">About</Link>
-          <Link to="/Vans">Vans</Link>
-        </nav>
-      </header>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/vans" element={<Vans />} />
-        <Route path="/vans/:id" element={<VanDetail />} />
-      </Routes>
-    </BrowserRouter>
-  )
-}
+      <BrowserRouter>
+        <Routes>
+          <Route element={<Layout />}>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/vans" element={<Vans />} />
+            <Route path="/vans/:id" element={<VanDetail />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    )
+  }
 
 export default App
